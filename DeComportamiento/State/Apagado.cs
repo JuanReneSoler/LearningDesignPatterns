9049765833605cf134c674e1263abb954672adcc
Juan Rene Soler <juanr.solerl_93@hotmail.es>
2022-01-09 00:43:45 -0400

    agregue un statod mas al patron state

--- DeComportamiento/State/Apagado.cs
+++ DeComportamiento/State/Apagado.cs
@@ -3,7 +3,7 @@ namespace patrones.State
 {
     public class Apagado : Estado
     {
-	public override void ControlarEstado(Switch sw) => sw.DefinirEstado(new Encendido());
+	public override void ControlarEstado(Switch sw) => sw.DefinirEstado(new StandBy());
 	public override string Describir() => "Switch apagado";
     }
 }
---
+++ DeComportamiento/State/StandBy.cs
@@ -0,0 +1,9 @@
+
+namespace patrones.State
+{
+    public class StandBy : Estado
+    {
+	public override void ControlarEstado(Switch sw) => sw.DefinirEstado(new Encendido()  );
+	public override string Describir() => "Switch en stand by";
+    }
+}
